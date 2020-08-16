f71a60ee0292ef0ceb8a89adeeddbef18f139378
bwmelon97 <bwmelon97@naver.com>
2020-08-16 10:41:41 +0000

    init app.tsx

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import Greetings from "./Greetings";
-import Counter from "./Counter";
-import MyForm, {Form} from "./MyForm";
-import ReducerSample from "./ReducerSample";
 
 function App() {
 
-    const onClick = (name: string) => console.log(name); 
-    const onSubmit = (form: Form) => console.log(form);
-
     return (
         <div>
-            <Greetings 
-                name="Soogeun" 
-                onClick={onClick}
-                // optional='hello world!'    
-            />
-
-            <Counter/>
-
-            <MyForm onSubmit={onSubmit} />
-
-            <ReducerSample />
+            Hello World !
         </div>
     );
 }
