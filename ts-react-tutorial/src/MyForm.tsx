43d7b1d52942e4c9d4402ed4c376dddf742d9e09
bwmelon97 <bwmelon97@naver.com>
2020-08-16 06:56:59 +0000

    Step 4 useRef

--- ts-react-tutorial/src/MyForm.tsx
+++ ts-react-tutorial/src/MyForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 interface MyFormProps {
     onSubmit: (form: Form) => void;
@@ -12,6 +12,7 @@ export type Form = {
 
 function MyForm ( {onSubmit}: MyFormProps ) {
     const [form, setForm] = useState<Form>({name:'', description: ''});
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const {name, description} = form;
 
@@ -27,11 +28,13 @@ function MyForm ( {onSubmit}: MyFormProps ) {
         e.preventDefault();
         onSubmit(form);
         setForm({ name: '', description: '' })
+        if (!inputRef.current) { return }
+        inputRef.current.focus();
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input name='name' value={name} onChange={onChange} ></input>
+            <input name='name' value={name} onChange={onChange} ref={inputRef} ></input>
             <input name='description' value={description} onChange={onChange} ></input>
             <button type='submit' >제출</button>
         </form>
