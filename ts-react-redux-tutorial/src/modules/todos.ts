517e1e04857325b0e195ec3c4488b567ed080a57
bwmelon97 <bwmelon97@naver.com>
2020-08-17 05:10:59 +0000

    rrt 2-3 TodoReducer 생성

--- ts-react-redux-tutorial/src/modules/todos.ts
+++ ts-react-redux-tutorial/src/modules/todos.ts
@@ -9,7 +9,7 @@ const toggleTodo = (id: number) => ({type: TOGGLE_TODO, payload: id})
 const removeTodo = (id: number) => ({type: REMOVE_TODO, payload: id})
 
 // 액션들의 타입스크립트 타입 준비
-type TodoAction = 
+type TodosAction = 
     ReturnType<typeof addTodo> |
     ReturnType<typeof toggleTodo> |
     ReturnType<typeof removeTodo> ;
@@ -22,16 +22,54 @@ type Todo = {
     done: boolean;
 }  
 
-type TodosState = Todo[];
-
+type TodosState = { todoList: Todo[]};
 
 // 초기 state 설정
-const initialState: TodosState = [
-  { id: 1, text: '타입스크립트 배우기', done: true },
-  { id: 2, text: '타입스크립트와 리덕스 함께 사용해보기', done: true },
-  { id: 3, text: '투두리스트 만들기', done: false }
-];
+const initialState: TodosState = {
+    todoList: [
+        { id: 1, text: '타입스크립트 배우기', done: true },
+        { id: 2, text: '타입스크립트와 리덕스 함께 사용해보기', done: true },
+        { id: 3, text: '투두리스트 만들기', done: false }
+    ]
+};
 
 
+/* Reducer */
+export default function todos ( state: TodosState = initialState, action: TodosAction ): TodosState {
+    switch (action.type) {
+        case ADD_TODO: {
+            const nextId: number = Math.max(0, ...state.todoList.map(todo => todo.id) ) + 1;
+            return {
+                ...state,
+                todoList: state.todoList.concat({
+                    id: nextId,
+                    text: action.payload,
+                    done: false
+                })
+            }
+        }
 
-export {}
+        case TOGGLE_TODO: {
+            return {
+                ...state,
+                todoList: state.todoList.map( todo => {
+                    return todo.id !== action.payload ? 
+                    {...todo, done: !todo.done} : todo;
+                })
+            }
+        }
+
+        case REMOVE_TODO: {
+            return {
+                ...state,
+                todoList: state.todoList.filter( todo => {
+                    return todo.id !== action.payload
+                })
+            }
+        }
+
+        default:
+            return state;
+    }
+}
+
