9de079c41fd889bb6efe41a8ccf7a2a92dc8319b
bwmelon97 <bwmelon97@naver.com>
2020-08-17 04:55:25 +0000

    rrt 2-2 TodosState 설정

--- ts-react-redux-tutorial/src/modules/todos.ts
+++ ts-react-redux-tutorial/src/modules/todos.ts
@@ -14,4 +14,24 @@ type TodoAction =
     ReturnType<typeof toggleTodo> |
     ReturnType<typeof removeTodo> ;
 
+
+// state를 위한 타입 설정
+type Todo = {
+    id: number;
+    text: string;
+    done: boolean;
+}  
+
+type TodosState = Todo[];
+
+
+// 초기 state 설정
+const initialState: TodosState = [
+  { id: 1, text: '타입스크립트 배우기', done: true },
+  { id: 2, text: '타입스크립트와 리덕스 함께 사용해보기', done: true },
+  { id: 3, text: '투두리스트 만들기', done: false }
+];
+
+
+
 export {}
