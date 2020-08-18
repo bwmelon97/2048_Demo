b47c12fb8a5e7bcc71e2ae1b0c76c255f05e2154
bwmelon97 <bwmelon97@naver.com>
2020-08-18 11:21:14 +0000

    add package styled-components && setup Extension

---
+++ demo2048/src/components/GameTable.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+/* Styled Components */
+const Container = styled.div`
+    
+`;
+
+/* React Component */
+function GameTable () {
+
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+
+export default GameTable;
