f5f226e42a33f8862b3abd462d3615cf3d7f0a8e
bwmelon97 <bwmelon97@naver.com>
2020-08-23 13:17:01 +0000

    Design Game Header Component

--- demo2048/src/components/GameHeader/GameHeader.tsx
+++ demo2048/src/components/GameHeader/GameHeader.tsx
@@ -6,7 +6,7 @@ import { color } from "../../styles/variables";
 
 /* Styled Components */
 const Container = styled.div`
-    padding-top: 200px;
+    padding-top: 120px;
     height: 200px;
 
     display: flex;
@@ -16,30 +16,73 @@ const Container = styled.div`
     const MainBox = styled.div`
         display: flex;
         flex-flow: row nowrap;
+        align-items: flex-start;
+        justify-content: space-between;
     `;
 
         const TitleBox = styled.div`
-            
+            font-size: 48px;
+            font-weight: bold;
+            color: ${color.text_title};
         `;
 
         const ExtrasContainer = styled.div`
-        
+            display: flex;
+            flex-flow: column nowrap;
         `;
         
             const ScoreContainer = styled.div`
-            
+                display: flex;
+                flex-flow: row nowrap;
             `;
 
                 const ScoreBox = styled.div`
+                    padding: 12px;
+                    margin: 8px;
+                    width: 64px;
                 
+                    background-color: ${color.header_score_box};
+                    border-radius: 4px;
+
+                    display: flex;
+                    flex-flow: column nowrap;
+                    align-items: center;
                 `;
 
+                    const ScoreName = styled.div`
+                        margin-bottom: 2px;
+
+                        color: ${color.text_header};
+                        font-size: 12px;
+                    `;
+
+                    const ScoreValue = styled.div`
+                        color: ${color.text_title};
+                        font-size: 18px;
+                        font-weight: bold;
+                    `;
+
             const NewGameButton = styled.div`
-            
+                padding: 12px;
+                margin: 8px;
+
+                background-color: ${color.header_new_game};
+                border-radius: 4px;
+                
+                color: ${color.text_header};
+                font-size: 18px;
+
+                cursor: pointer;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
             `;
 
     const DescriptionBox = styled.div`
-    
+        margin-top: 40px;
+
+        color: ${color.text_description};
     `; 
 
 
@@ -51,8 +94,14 @@ function GameHeader () {
                 <TitleBox> 2048 </TitleBox>
                 <ExtrasContainer>
                     <ScoreContainer>
-                        <ScoreBox></ScoreBox>
-                        <ScoreBox></ScoreBox>
+                        <ScoreBox>
+                            <ScoreName>Score</ScoreName>
+                            <ScoreValue>1000</ScoreValue>
+                        </ScoreBox>
+                        <ScoreBox>
+                            <ScoreName>Best</ScoreName>
+                            <ScoreValue>10000</ScoreValue>
+                        </ScoreBox>
                     </ScoreContainer>
 
                     <NewGameButton> New Game </NewGameButton>
