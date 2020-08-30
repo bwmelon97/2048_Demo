fe1b4ebf02af5e0aeee3177f4850e7d295c2d901
bwmelon97 <bwmelon97@naver.com>
2020-08-30 05:28:28 +0000

    add package react-devtools-extension

--- learn-redux-middleware/src/index.tsx
+++ learn-redux-middleware/src/index.tsx
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import rootReducer from "./modules";
-import { myLogger } from "./middlewares";
+// import { myLogger } from "./middlewares";
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
     <React.StrictMode>
